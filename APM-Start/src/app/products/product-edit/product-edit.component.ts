8c3a83dd59d64223beec6f8c22d8f271ac0b9157
David <david.martin@est.tech>
2019-05-28 12:41:07 +0100

    hooked up the edit product details component using the activated route service snapshit feature to access the product id

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../../messages/message.service';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit {
+
   pageTitle = 'Product Edit';
   errorMessage: string;
 
   product: Product;
 
   constructor(private productService: ProductService,
-              private messageService: MessageService) { }
+              private messageService: MessageService,
+              private activatedRoute: ActivatedRoute) { }
 
   getProduct(id: number): void {
     this.productService.getProduct(id)
@@ -81,5 +84,10 @@ export class ProductEditComponent {
     }
 
     // Navigate back to the product list
+  }
+
+  ngOnInit(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.getProduct(id);
   }
 }
